f183ef1b243805fe0dbb8b40998d289a218397bc
MRIkaito <96245944+MRIkaito@users.noreply.github.com>
2024-01-28 22:39:10 +0900

    Made MemoDetailWindow's file. Made MemoDetailWindow MarkUp.

---
+++ src/app/memo/list.tsx
@@ -0,0 +1,28 @@
+import { View, StyleSheet } from 'react-native'
+
+import Header from '../../components/Header'
+import MemoListItems from '../../components/MemoListItems'
+import CircleButton from '../../components/CircleButton'
+
+const List = (): JSX.Element => {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View>
+        <MemoListItems />
+        <MemoListItems />
+        <MemoListItems />
+      </View>
+      <CircleButton>＋</CircleButton>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff'
+  }
+})
+
+export default List
