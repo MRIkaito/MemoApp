77f60454ebc31d650c58abd0106a8875c7287942
MRIkaito <96245944+MRIkaito@users.noreply.github.com>
2024-02-01 23:33:51 +0900

    Remove Header.tsx Add Header-layout

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,7 +1,6 @@
 import { View, StyleSheet } from 'react-native'
 import { router } from 'expo-router'
 
-import Header from '../../components/Header'
 import MemoListItems from '../../components/MemoListItems'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/Icon'
@@ -13,7 +12,6 @@ const handlePress = (): void => {
 const List = (): JSX.Element => {
   return (
     <View style={styles.container}>
-      <Header />
       <View>
         <MemoListItems />
         <MemoListItems />
