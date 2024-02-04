760ed9e90bf4b03fd7cb98008685dfc18c4d0e63
MRIkaito <96245944+MRIkaito@users.noreply.github.com>
2024-02-04 09:14:52 +0900

    Replace function react-native-component's KeyboardAvoidingView from original's Implement keyboard autofocus

--- src/app/memo/create.tsx
+++ src/app/memo/create.tsx
@@ -1,9 +1,10 @@
 import {
-  View, TextInput, StyleSheet, KeyboardAvoidingView
+  View, TextInput, StyleSheet
 } from 'react-native'
 import { router } from 'expo-router'
 import { collection, addDoc, Timestamp } from 'firebase/firestore'
 
+import KeyboardAvoidingView from '../../components/KeyboardAvoidingView'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/Icon'
 import { db, auth } from '../../config'
@@ -30,13 +31,14 @@ const handlePress = (bodyText: string): void => {
 const Create = (): JSX.Element => {
   const [bodyText, setBodyText] = useState('')
   return (
-    <KeyboardAvoidingView behavior='height' style={styles.container}>
+    <KeyboardAvoidingView style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput
           multiline
           style={styles.input}
           value={bodyText}
           onChangeText = {(text) => { setBodyText(text) }}
+          autoFocus
           />
       </View>
       <CircleButton onPress = { () => { handlePress(bodyText) } }>
